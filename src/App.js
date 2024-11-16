abb19cb129f42705cff82cdbacd336745ba53d94
Jorge <jorgenizamarios@gmail.com>
2024-11-16 04:57:09 +0100

    update app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprendiendo React a tiempo completo
         </a>
       </header>
     </div>
